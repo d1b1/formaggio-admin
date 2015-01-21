4eef90105a3988023058d0bfe5a36fefc529bb88
Stephan Smith 2 <ssmith@adsl-172-10-1-54.dsl.sndg02.sbcglobal.net>
2015-01-21 08:23:39 -0500

    Added in the pushstate

--- app/app.js
+++ app/app.js
@@ -100,24 +100,22 @@ window.Session = new Session();
 window.Session.setup();
 
 // All navigation that is relative should be passed through the navigate
-// method, to be processed by the router. If the link has a `data-bypass`
+// method, to be processed by the router.  If the link has a data-bypass
 // attribute, bypass the delegation completely.
 $(document).on("click", "a:not([data-bypass])", function(evt) {
-  // Get the absolute anchor href.
-  alert('asdf');
-  var href = $(this).prop("href");
-  // Get the absolute root.
-  var root = location.protocol + "//" + location.host + app.root;
+  // Get the anchor href and protcol
+  var href = $(this).attr("href");
+  var protocol = this.protocol + "//";
 
-  // Ensure the protocol is not part of URL, meaning it's relative.
-  if (href && href.indexOf(root)) {
-    alert('got here');
-    // Stop the default event to ensure the link will not cause a page refresh.
+  // Ensure the protocol is not part of URL, meaning its relative.
+  if (href && href.slice(0, protocol.length) !== protocol && href.indexOf("javascript:") !== 0) {
+    // Stop the default event to ensure the link will not cause a page
+    // refresh.
     evt.preventDefault();
 
     // `Backbone.history.navigate` is sufficient for all Routers and will
-    // trigger the correct events. The Router's internal `navigate` method
-    // calls this anyways.  The fragment is sliced from the root.
-    Backbone.history.navigate(href.slice(root.length), true);
+    // trigger the correct events.  The Router's internal `navigate` method
+    // calls this anyways.
+    Backbone.history.navigate(href, true);
   }
 });
